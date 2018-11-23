77662c78805fd18d418dda1d415404d2a9099444
edoBaldini <edoardo-baldini@virgilio.it>
2018-11-23 21:55:09 +0100

    Added-Check-on-Dataservice-Connection

--- beepbeep/challenge_microservice/views/swagger.py
+++ beepbeep/challenge_microservice/views/swagger.py
@@ -1,28 +1,24 @@
-import os, requests
+import os, requests, json
 from datetime import datetime, date
-from .util import bad_response, generate_dataservice_url
+from .util import bad_response
 from flakon import SwaggerBlueprint
-from flask import request, jsonify, redirect
+from flask import request, jsonify, redirect, abort
 from beepbeep.challenge_microservice.database import db, Challenge
-import json
 from json import loads
-
+from flakon.util import retry_request
+from flakon.request_utils import users_endpoint, get_request, runs_endpoint, put_request_retry
 
 HERE = os.path.dirname(__file__)
 YML = os.path.join(HERE, '..', 'static', 'api.yaml')
 api = SwaggerBlueprint('API', __name__, swagger_spec=YML)
-URL_DATASERVICE = generate_dataservice_url()
-
 
 def check_user(runner_id):
-    url = URL_DATASERVICE + '/user/' + str(runner_id)
-    r = requests.get(url)
+    r = get_request(users_endpoint(), runner_id)
     result = r.json()
     return 'id' in result
 
 def get_single_run(runner_id, run_id):
-    url = URL_DATASERVICE + '/user/' + str(runner_id) + '/runs/' + str(run_id)
-    r = requests.get(url)
+    r = get_request(runs_endpoint(runner_id), run_id)
     result = r.json()
     return result
 
@@ -38,28 +34,31 @@ def date_parsing(date):
 @api.operation('createChallenge')
 def create_challenge():
     challenge = request.get_json()
-    run = get_single_run(challenge['runner_id'], challenge['run_challenged_id'])
-    if check_user(challenge['runner_id']) and 'id' in run:
-        if run['runner_id'] == challenge['runner_id']:
-            db_challenge = Challenge()
-            db_challenge.runner_id = challenge['runner_id']
-            db_challenge.run_challenged_id = challenge['run_challenged_id']
-            db_challenge.start_date = datetime.today()
-            db.session.add(db_challenge)
-            db.session.commit()
-            return "added 1 challenge", 204
+    try:
+        run = get_single_run(challenge['runner_id'], challenge['run_challenged_id'])
+        if check_user(challenge['runner_id']) and 'id' in run:
+            if run['runner_id'] == challenge['runner_id']:
+                db_challenge = Challenge()
+                db_challenge.runner_id = challenge['runner_id']
+                db_challenge.run_challenged_id = challenge['run_challenged_id']
+                db_challenge.start_date = datetime.today()
+                db.session.add(db_challenge)
+                db.session.commit()
+                return "added 1 challenge", 204
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return abort(503) # SERVICE UNAVAILABLE
     return bad_response(400, 'No user with ID ' + str(challenge['runner_id']) + 'and run' + str(challenge['run_challenged_id']))
 
 
 @api.operation('getChallenges')
 def get_challenges(runner_id):
-    url = URL_DATASERVICE + '/user/' + str(runner_id)
-    r = requests.get(url)
-    result = r.json()
-    if 'id' in result:
-        challenges = db.session.query(Challenge).filter(Challenge.runner_id == runner_id)
+    challenges = db.session.query(Challenge).\
+                    filter(Challenge.runner_id == runner_id)
+    if challenges.count() > 0:
         return jsonify([challenge.to_json() for challenge in challenges])
-    return bad_response(404, 'No user with ID ' + str(runner_id))
+    return bad_response(404, 'No challenge for user with ID ' + str(runner_id))
+
 
 
 @api.operation('getChallengeID')
@@ -75,7 +74,11 @@ def complete_challenge(runner_id, challenge_id):
     run_challenger_id = request.get_json()['run_challenger_id']
     if challenge is not None:
         if challenge.run_challenger_id is None:
-            run = get_single_run(runner_id, run_challenger_id)
+            try:
+                run = get_single_run(runner_id, run_challenger_id)
+            except requests.exceptions.RequestException as err:
+                print(err)
+                return abort(503)
             if run is not None and datetime.fromtimestamp(run['start_date']) > challenge.start_date:
                 challenge.run_challenger_id = run_challenger_id
                 challenge.result = determine_result(challenge, run)
@@ -86,7 +89,11 @@ def complete_challenge(runner_id, challenge_id):
     else: return bad_response(404, 'No challenge with ID ' + str(challenge_id) + ' for user with ID ' + str(runner_id))
 
 def determine_result(challenge, current_run):
-    challenged_run = get_single_run(challenge.runner_id, challenge.run_challenged_id)
+    try:
+        challenged_run = get_single_run(challenge.runner_id, challenge.run_challenged_id)
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return abort(503)
     if challenged_run is not None:
         if challenged_run['distance'] == current_run['distance']:
             return challenged_run['average_speed'] < current_run['average_speed']
@@ -102,15 +109,3 @@ def delete_user_challenges(runner_id):
         db.session.delete(challenge)
         db.session.commit()
     return "challenges removed", 200
-
-#@api.operation('completeChallenge')
-#def complete_challenge(runner_id, challenge_id):
-#    challenge = get_challenge_of_runner_id(runner_id, challenge_id)
-#    if challenge is not None:
-#        if challenge.run_challenger_id is None:
-#            url = URL_DATASERVICE + '/user/' + str(runner_id) + '/runs'
-#            r = requests.get(url, params= {'start-date': date_parsing(challenge.start_date)})
-#            results = r.json()
-#            return jsonify([result for result in results])
-#        else: return jsonify(challenge.to_json())
-#    else: return bad_response(404, 'No challenge with ID ' + str(challenge_id) + ' for user with ID ' + str(runner_id))
